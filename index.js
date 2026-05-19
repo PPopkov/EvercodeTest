925143f68c9d68158830c4f9d249a46c6e1e0e2f
Pavel Popkov <goil2011@yandex.ru>
2026-05-19 17:22:08 +0300

    fix: fix log call and add error handling in index.js

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const { scheduleTask } = require("./src/scheduler");
 const { log } = require("./src/logger");
 
 scheduleTask("Timer", 1000, () => {
-  log("running");
+  log.info("running");
 });
+
+try {
+  scheduleTask(123, 1000, log.info);
+} catch (error) {
+  log.error(`${error.name}: ${error.message}`);
+}
