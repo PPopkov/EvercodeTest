8ccf1c76deb96145f705b13eabbbdab2f35d2239
Pavel Popkov <goil2011@yandex.ru>
2026-05-24 15:20:19 +0300

    fix: replace glob pattern with explicit route paths in swagger config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,26 +1,31 @@
-const swaggerJsdoc = require('swagger-jsdoc');
-const path = require('path');
+const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: { 
-            title: 'Currency API', 
-            version: '1.0.0',
-            description: 'API for managing currencies'
-        },
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer'
-                }
-            }
-        }
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Currency API",
+      version: "1.0.0",
+      description: "API for managing currencies",
     },
-    apis: [path.join(__dirname, '../routes/*.js')]
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+        },
+      },
+    },
+  },
+  apis: [
+    path.join(__dirname, "../routes/currency.js"),
+    path.join(__dirname, "../routes/status.js"),
+    path.join(__dirname, "../routes/price.js"),
+  ],
 };
 
 const spec = swaggerJsdoc(options);
-
+console.log(path.join(__dirname, "../routes/*.js"));
+console.log(JSON.stringify(spec, null, 2));
 module.exports = spec;
