47a1476317ebabb909c0dee870338b3e44cfa0ca
Pavel Popkov <goil2011@yandex.ru>
2026-05-30 14:37:44 +0300

    fix: update tests for createApp factory and DI

--- __tests__/currency.test.js
+++ __tests__/currency.test.js
@@ -1,11 +1,13 @@
 const request = require("supertest");
-const app = require("../src/app");
-const currencyService = require("../src/services/currencyService");
-
 process.env.AUTH_TOKEN = "test-token-123";
 
+const { createApp } = require("../src/app");
+const { createCurrencyService } = require("../src/services/currencyService");
+let app;
+
 beforeEach(() => {
-  currencyService.reset();
+  const currencyService = createCurrencyService();
+  app = createApp(currencyService, {});
 });
 
 test("/GET currency return empty array", async () => {
--- __tests__/status.test.js
+++ __tests__/status.test.js
@@ -1,7 +1,9 @@
 const request = require("supertest");
-const app = require("../src/app");
-
 process.env.AUTH_TOKEN = "test-token-123";
+const {createApp} = require("../src/app");
+const { createCurrencyService } = require("../src/services/currencyService");
+let app = createApp(createCurrencyService(), {});
+
 
 test("GET /status return ok", async () => {
   const res = await request(app)
