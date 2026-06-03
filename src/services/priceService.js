cca68469643dcd78a346291f66da0bd2b91c3ce0
Pavel Popkov <goil2011@yandex.ru>
2026-06-03 22:50:15 +0300

    feat: add priceService

---
+++ src/services/priceService.js
@@ -0,0 +1,33 @@
+const { NotFoundError } = require("../errors/NotFoundError");
+
+function createPriceService(currencyRepository, priceRepository, binanceService) {
+  const self = {
+    getCurrency: (ticker) => {
+      if (!currencyRepository.getByTicker(ticker)) {
+        throw new NotFoundError("Not Found");
+      }
+      const prices = priceRepository.getBySymbol(ticker);
+      return prices;
+    },
+    syncPrices: async() => {
+      const currencies = currencyRepository.getAll();
+      for (const currency of currencies) {
+        const routes =  await binanceService.getByTicker(currency.ticker);
+        for (const route of routes) {
+          priceRepository.savePrice(
+            route.symbol,
+            route.price,
+            new Date().toISOString()
+          );
+        }
+      }
+
+    }
+  };
+
+  return self;
+}
+
+module.exports = {
+  createPriceService,
+};
