5924b9f52909570a9b2bc5db1c8360329eff6ca4
Pavel Popkov <goil2011@yandex.ru>
2026-06-12 14:33:11 +0300

    feat: add entity types (Currency, Price, BinanceTicker)

---
+++ src/types/entities/price.ts
@@ -0,0 +1,6 @@
+export interface Price {
+    id: number,
+    symbol: string,
+    price: number,
+    updated_at: string
+}
---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export { Currency } from "./entities/currency";
+export { Price } from "./entities/price";
+export { BinanceTicker } from "./api/binance";
